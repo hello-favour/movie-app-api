b528313a9dd520041e8c86b7af367b7f4604071f
Favour <hello.williamsfav@gmail.com>
2023-05-16 10:36:26 +0100

    set navgation to the description screen

--- lib/screens/description_screen.dart
+++ lib/screens/description_screen.dart
@@ -23,6 +23,16 @@ class DescriptionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.appBlack,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+          ),
+        ),
+      ),
       body: Container(
         child: ListView(
           children: [
